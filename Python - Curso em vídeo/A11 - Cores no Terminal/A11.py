1d1f0febf59d6f1d85f804a59492b12e47d68d61
João Vaz <vaz.contato7@gmail.com>
2026-02-16 17:32:43 -0300

    "Subindo meus arquivos Python"

---
+++ Python - Curso em vídeo/A11 - Cores no Terminal/A11.py
@@ -0,0 +1,34 @@
+# \033(0:33:44m -> Dentro dos colchetes [estilo;cor da letra;cor de fundo]
+'''
+ESTILOS:
+0 = Nada
+1 = Negrito
+4 = Sublinhado
+7 = Inverte cor de texto com fundo
+
+CORES DE LETRA:
+30 = branco
+31 = vermelho
+32 = verde
+33 = amarelo
+34 = azul
+35 = roxo
+36 = ciano
+37 = cinza
+
+CORES DE FUNDO:
+40 = branco
+41 = vermelho
+42 = verde
+43 = amarelo
+44 = azul
+45 = roxo
+46 = ciano
+47 = cinza
+'''
+
+print('\033[1;30;46mTeste')
+print('\033[7;30;46mTeste\033[m')
+print('\033[4;31;30mTeste\033[m')
+print('\033[;34;44mTeste\033[m')
+print('\033[7;35;47mTeste\033[m')
---
+++ Python - Curso em vídeo/Testes/Teste Python Mundo 1.py
@@ -0,0 +1,38 @@
+# Q1:
+s = 'prova de python'
+print(s)
+print(len(s))
+
+# Q2:
+import math
+from math import sqrt
+raiz = sqrt(25)
+print(raiz)
+
+# Q3:
+'''Programa de TV'''
+
+# Q4:
+'''double'''
+
+# Q5:
+'''1991 / Guido Van Hossum'''
+
+# Q6:
+x = 'curso de python no cursoemvideo'
+print(x[:5])
+
+# Q7:
+nome = 'José'
+idade = 25
+print(f'Você se chama {nome} e tem {idade} anos de idade.')
+
+# Q8:
+'''nativamente compilada'''
+
+# Q9:
+eq = (3*5)+(4**2)
+print(eq)
+
+# Q10:
+'''bool / True / False'''
